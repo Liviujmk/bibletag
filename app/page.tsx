9bd2fa4250cb1eb2e071cfe85ab5a55ef216c2da
Liviu Mitrofan <liviujmk@gmail.com>
2024-02-01 23:18:01 +0200

    add

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="text-8xl mb-4">BibleTag</h1>
+      <h1 className="text-8xl mb-10">BibleTag</h1>
       <Link href="/articles" className="block text-5xl underline border-l-pink-500">Articles</Link>
       <Link href="/categories" className="text-5xl underline border-l-pink-500">Categories</Link>
     </>
